add6775df85d51bd711b1a906108f229245e947b
singhpriansh <priyanshu.h786@gmail.com>
2021-10-31 00:52:51 +0530

    database added

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const db = "mongodb+srv://secureuser:FCIokCNFUgMqkelI@cluster0.c7q1k.mongodb.net/node-angular?retryWrites=true&w=majority"
+mongoose.connect(db)
+    .then(() => {
+        console.log('Connected to the database!')
+    })
+    .catch(() => {
+        console.log('Connection failed!')
+    });
+
 
 router.get("/",(req,res)=>{
     res.send("From API route")
