1f1193e4d00e5c95697edd6b6f8b4491f82e09c7
singhpriansh <priyanshu.h786@gmail.com>
2021-11-01 15:55:29 +0530

    components initialised

--- ngevents/src/app/app-routing.module.ts
+++ ngevents/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SpecialComponent } from './special/special.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'/events', pathMatch:'full' },
+  { path:'events', component:EventsComponent },
+  { path:'special', component:SpecialComponent },
+  { path:'login', component:LoginComponent },
+  { path:'register', component:RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ngevents/src/app/app.module.ts
+++ ngevents/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialComponent } from './special/special.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    EventsComponent,
+    SpecialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
